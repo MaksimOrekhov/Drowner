cd2af0fc173e63c6bed41ec395234063ac462274
Руслан Королев <techfusionwork@gmail.com>
2019-10-20 19:41:25 +0300

    cordova and phaser

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-browsersync": "0.1.7"
+}
+// BOTTOM OF METADATA
+});
