12c34ccccf8d295bd5df5d10f586f92fa77cc0c7
Руслан Королев <techfusionwork@gmail.com>
2019-10-29 21:34:29 +0300

    g

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,16 +1,17 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
 export default class Game extends Phaser.Scene {
     constructor() {
-        super('Game')
-        this.pet = null
-        this.petAge = 0
+        super('Game');
+        this.pet = null;
+        this.petAge = 0;
+        this.fulness = 100;
         this.growthStages = {
             child: 0,
             teenager: 1,
             grownUp: 3,
             death: 5,
-        }
+        };
     }
 
     init() {}
@@ -19,56 +20,76 @@ export default class Game extends Phaser.Scene {
         this.load.spritesheet('child', '../assets/images/mushroom2.png', {
             frameWidth: 64,
             frameHeight: 64,
-        })
+        });
         this.load.spritesheet('teenager', '../assets/images/icon-192px.png', {
             frameWidth: 192,
             frameHeight: 192,
-        })
+        });
         this.load.spritesheet('grownUp', '../assets/images/icon-512px.png', {
             frameWidth: 512,
             frameHeight: 512,
-        })
-        this.load.image('death', '../assets/images/phaser.png')
+        });
+        this.load.image('death', '../assets/images/phaser.png');
     }
 
     create() {
         this.time.addEvent({
-            delay: 3000,
+            delay: 30000,
             callback: this.updateAge,
             callbackScope: this,
             loop: true,
-        })
-        this.pet = this.add.sprite(400, 400, 'child')
+        });
+        this.time.addEvent({
+            delay: 100,
+            callback: this.updateFulness,
+            callbackScope: this,
+            loop: true,
+        });
+        this.pet = this.add.sprite(400, 400, 'child');
     }
 
     update() {}
 
     updateAge() {
-        this.petAge += 1
+        this.petAge += 1;
 
         switch (this.petAge) {
             case this.growthStages.teenager:
-                this.pet.setTexture('teenager') // меняем текстуру спрайта
-                break
+                this.pet.setTexture('teenager'); // меняем текстуру спрайта
+                break;
             case this.growthStages.grownUp:
-                this.pet.setTexture('grownUp')
-                break
+                this.pet.setTexture('grownUp');
+                break;
             case this.growthStages.death:
-                this.pet.setTexture() // вставляем пустую текстуру
-                this.add.image(400, 400, 'death')
+                this.pet.setTexture(); // вставляем пустую текстуру
+                this.add.image(400, 400, 'death');
                 this.time.addEvent({
                     delay: 3000,
                     callback: this.showGameOverScene,
                     callbackScope: this,
                     loop: false,
-                })
+                });
 
-                this.petAge = 0
-                break
+                this.petAge = 0;
+                break;
+        }
+    }
+
+    updateFulness() {
+        this.fulness -= 1;
+        console.log(this.fulness);
+        if (this.fulness <= 50) {
+            this.add.text(100, 100, 'Ваш питомец на половину голоден!', {
+                font: '40px Bangers',
+                fill: '#7744ff',
+            });
+        }
+        if (!this.fulness) {
+            this.scene.start('GameOver');
         }
     }
 
     showGameOverScene() {
-        this.scene.start('GameOver')
+        this.scene.start('GameOver');
     }
 }
