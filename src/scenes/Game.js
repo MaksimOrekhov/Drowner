810a70fd0a0a64490eddee3eafca999c7a17c188
Maksim Orekhov <timberwolf.max@gmail.com>
2019-10-30 22:33:45 +0300

    Trying to create animations

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -17,20 +17,10 @@ export default class Game extends Phaser.Scene {
     init() {}
 
     preload() {
-        this.load.path = 'assets/images/';
-        this.load.image('zombie1', 'character_zombie_walk0.png');
-        this.load.image('zombie2', 'character_zombie_walk1.png');
-        this.load.image('zombie3', 'character_zombie_walk2.png');
-        this.load.image('zombie4', 'character_zombie_walk3.png');
-        this.load.image('zombie5', 'character_zombie_walk4.png');
-        this.load.image('zombie6', 'character_zombie_walk5.png');
-        this.load.image('zombie7', 'character_zombie_walk6.png');
-        this.load.image('zombie8', 'character_zombie_walk7.png');
-
-        // this.load.spritesheet('child', 'assets/images/zombie.png', {
-        //     frameWidth: 192,
-        //     frameHeight: 256,
-        // });
+        this.load.spritesheet('child', 'assets/images/bird.png', {
+            frameWidth: 128,
+            frameHeight: 128,
+        });
         // this.load.spritesheet('teenager', 'assets/images/icon-192px.png', {
         //     frameWidth: 192,
         //     frameHeight: 192,
@@ -55,26 +45,15 @@ export default class Game extends Phaser.Scene {
         //     callbackScope: this,
         //     loop: true,
         // });
-        // this.pet = this.add.sprite(200, 200, 'child');
+        this.pet = this.add.sprite(200, 200, 'child');
 
         this.anims.create({
             key: "child_anim",
-            // frames: this.anims.generateFrameNumbers("child"),
-            frames: [
-                { key: 'zombie1', frame: null },
-                { key: 'zombie2', frame: null },
-                { key: 'zombie3', frame: null },
-                { key: 'zombie4', frame: null },
-                { key: 'zombie5', frame: null },
-                { key: 'zombie6', frame: null },
-                { key: 'zombie7', frame: null },
-                { key: 'zombie8', frame: null, duration: 50 }
-            ],
+            frames: this.anims.generateFrameNumbers("child"),
             frameRate: 15,
             repeat: -1
         })
-        this.add.sprite(200, 200, 'zombie1').play('child_anim');
-        // this.pet.play("child_anim")
+        this.pet.play("child_anim")
     }
 
     update() {}
