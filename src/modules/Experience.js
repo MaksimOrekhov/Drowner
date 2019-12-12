d3b9b42f4c7f2ec4f6b3f01287c07908dfeacc7f
Руслан Королев <techfusionwork@gmail.com>
2019-12-12 22:55:00 +0300

    exp

---
+++ src/modules/Experience.js
@@ -0,0 +1,10 @@
+
+// модуль получает информацию о совершенном действии
+// в зависимости от типа действия модуль выдает вознаграждение
+class Experience {
+    constructor(scene) {
+        this.scene = scene;
+    }
+}
+
+module.exports = Experience;
